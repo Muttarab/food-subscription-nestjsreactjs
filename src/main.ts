2864b1b5bfacdd8ac4a815f9c56d6bc7338d3584
Muttarab Ahmad <muttarabahmad9@gmail.com>
2022-04-06 06:42:12 +0500

    make it better

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from "cookie-parser";
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import path from 'path';
 require('dotenv').config({ path: '.env' })
 async function bootstrap() {
   const PORT = process.env.PORT || 8000
@@ -15,11 +14,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api', app, document);
   app.use(cookieParser());
-  if (process.env.NODE_ENV === "production") {
-    app.use(path.join(__dirname, "client/build"));
-  }
-  console.log(__dirname);
-  console.log(path.join(__dirname, "client/build"));
   app.enableCors({
     origin: 'http://localhost:3000',
     credentials: true
