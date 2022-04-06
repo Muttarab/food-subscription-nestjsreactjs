47c808b369c600bd51992c076c063392fac489f5
Muttarab Ahmad <muttarabahmad9@gmail.com>
2022-04-06 06:58:51 +0500

    make it better

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,8 +14,8 @@ import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 @Module({
   imports: [ConfigModule.forRoot(), ServeStaticModule.forRoot({
-    rootPath: join(__dirname,'..','client/build'),
-  }), TypeOrmModule.forRoot(typeOrmConfig), MulterModule.register({
+    rootPath: join(__dirname, '../../../', 'client/build'),
+}),, TypeOrmModule.forRoot(typeOrmConfig), MulterModule.register({
     dest: './files',
   }), ClientModule, PaymentModule, PaidinvoiceModule, WeeklymenuModule, AdminModule],
   controllers: [AppController],
